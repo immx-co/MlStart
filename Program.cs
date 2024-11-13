46eda02afa757afcbae1718cf05dbfb01fcc215e
Вячеслав Орлов <slava.orlov.2001@mail.ru>
2024-11-13 18:28:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+
+namespace MlStart
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = "MLstartConfig.json";
+            var content = @"{""Test"": ""123""}";
+            File.WriteAllText(fileName, content);
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory
+                .GetCurrentDirectory())
+                .AddJsonFile(fileName).Build();
+
+            string testConfig = configuration["Test"];
+            Console.WriteLine(testConfig);
+        }
+    }
+}
